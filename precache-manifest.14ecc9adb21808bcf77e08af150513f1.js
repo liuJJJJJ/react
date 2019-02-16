fdd1a49e20e7433fc35242d468f0cf63281555a4
liuJJJJJ <1058350816@qq.com>
2019-02-16 17:48:21 +0800

    Updates

---
+++ precache-manifest.14ecc9adb21808bcf77e08af150513f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "30ce0204bf44206d21eceae7aacd419a",
+    "url": "/react/static/media/ui-box-bg.30ce0204.30ce0204.png"
+  },
+  {
+    "revision": "d8986857e94c852b43b64318ae650c69",
+    "url": "/react/static/media/banner3.d8986857.png"
+  },
+  {
+    "revision": "17bad82ef3529110f080524b087ce9c0",
+    "url": "/react/static/media/banner2.17bad82e.png"
+  },
+  {
+    "revision": "82ec643ccebcfe4d1da786788d16b88d",
+    "url": "/react/static/media/banner1.82ec643c.png"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "cf0d68db91b1de947b60",
+    "url": "/react/static/js/main.cf0d68db.chunk.js"
+  },
+  {
+    "revision": "dbd4faee7ee04e32130d",
+    "url": "/react/static/js/1.dbd4faee.chunk.js"
+  },
+  {
+    "revision": "cf0d68db91b1de947b60",
+    "url": "/react/static/css/main.f790b4a3.chunk.css"
+  },
+  {
+    "revision": "b0c858fb62ddaf7b009313a05882f1b7",
+    "url": "/react/index.html"
+  }
+];
